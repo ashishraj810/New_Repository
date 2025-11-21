e05e95eea973e0dafd3fc5f055d8357d2d6022d0
ashishraj810 <ashishraj08105@gmail.com>
2025-11-21 20:28:20 +0530

    Add files via upload

---
+++ Linear_Search.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+bool linearsearch(int arr[] , int X , int index , int N)
+{
+  if(index == N)
+  return 0;
+
+  if(arr[index] == X)
+  return 1;
+
+  return linearsearch(arr , X , index+1 , N);
+}
+
+int main(){
+  int arr[] = {2,24,46,78,45,67};
+  int X = 45;
+  cout<<linearsearch(arr , X , 0 , 6)<<endl;
+}
