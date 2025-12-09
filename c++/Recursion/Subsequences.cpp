da416dc84df6ce901542978b58d1b0b3a5b2b120
ashishraj810 <ashishraj08105@gmail.com>
2025-12-09 12:39:12 +0530

    Add files via upload

---
+++ c++/Recursion/Subsequences.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+void subsequence(int arr[] , int index , int n ,vector<vector<int> >&ans , vector<int>temp )
+{
+  if(index == n)
+  {
+    ans.push_back(temp);
+    return;
+  }
+
+    //Not included
+    subsequence(arr , index+1 , n , ans , temp);
+
+    //Included
+    temp.push_back(arr[index]); 
+    subsequence(arr , index+1 , n , ans , temp);
+ 
+}
+
+int main()
+{
+  int arr[] = {2,5,6,1};
+  vector<vector<int> >ans;
+  vector<int>temp;
+  subsequence(arr , 0  , 4 , ans , temp);
+  for(int i=0 ; i<ans.size() ; i++)
+  {
+    for(int j=0 ; j<ans[i].size() ; j++)
+    {
+      cout<<ans[i][j]<<" ";
+      
+    }
+    cout<<endl;
+  }
+}
